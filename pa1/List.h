c3ca54925263766cd6c6aded1ab7e1bb829e5a65
Benjamin Swanzey <swanzeyb2001@gmail.com>
2023-09-15 23:08:15 -0700

    Working push and get operator functions for list

---
+++ pa1/List.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <memory>
+
+using std::shared_ptr;
+using std::make_shared;
+
+template <typename Type>
+class Node {
+  public:
+    shared_ptr<Node> next;
+    Type data;
+
+    Node() {};
+    ~Node() {};
+};
+
+template <typename Type>
+class List {
+  private:
+    shared_ptr<Node<Type>> head;
+    int size;
+  
+  public:
+    List(): head(nullptr), size(0) {};
+    ~List() {};
+
+    int push(Type data) {
+      // Make new node and initialize it
+      auto node = make_shared<Node<Type>>();
+      node->data = data;
+      node->next = nullptr;
+
+      // Find where to insert the node
+      if (head == nullptr) {
+        head = node;
+      } else {
+        auto current = head;
+        while (current->next != nullptr) {
+          current = current->next;
+        }
+        current->next = node;
+      }
+
+      size++;
+      return size;
+    }
+
+    // void splice();
+    Type* operator[](int index) {
+      auto current = head;
+      for (int i = 0; i < index; i++) {
+        if (current == nullptr) {
+          return nullptr; // Return null if index is out of bounds
+        }
+        current = current->next;
+      }
+      return &(current->data);
+    }
+};
--- pa1/main.cpp
+++ pa1/main.cpp
@@ -5,12 +5,23 @@
 #include "Menu.h"
 #include "Layout.h"
 #include "Lifecycle.h"
+#include "List.h"
 
 using std::cout;
 using std::endl;
 using std::shared_ptr;
 
 int main(int, char**) {
-  Menu* menu = new Menu();
-  Lifecycle lifecycle = { menu };
+  // Menu* menu = new Menu();
+  // Lifecycle lifecycle = { menu };
+
+  List<int> list;
+  list.push(1);
+  list.push(2);
+  list.push(3);
+
+  auto res1 = list[0];
+  auto res2 = list[1];
+  auto res3 = list[2];
+  auto res4 = list[3];
 }
