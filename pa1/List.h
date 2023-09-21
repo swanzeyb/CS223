f1f38e42db6dd06cdc20fdf06e7d21ea499bf300
Benjamin Swanzey <swanzeyb2001@gmail.com>
2023-09-20 19:29:26 -0700

    Node doesn't need to be public

--- pa1/List.h
+++ pa1/List.h
@@ -6,20 +6,20 @@ using std::make_shared;
 using std::shared_ptr;
 
 template <typename Type>
-class Node
-{
-public:
-  shared_ptr<Node> next;
-  Type data;
-
-  Node(){};
-  ~Node(){};
-};
-
-template <typename Type>
 class List
 {
 private:
+  template <typename Type>
+  class Node
+  {
+  public:
+    shared_ptr<Node> next;
+    Type data;
+
+    Node(){};
+    ~Node(){};
+  };
+
   shared_ptr<Node<Type>> head;
 
 public:
