28bb7e252f9a8b3d2db6e5478ca1e16a4df6b8ff
Benjamin Swanzey <swanzeyb2001@gmail.com>
2023-09-28 21:36:31 -0700

    Add BST destructor

--- MA2_start_code/BST.h
+++ MA2_start_code/BST.h
@@ -5,18 +5,19 @@
 #include <limits>
 using namespace std;
 
-
 /*
  *  Data structure for a single tree node
  */
 template <typename T>
-struct Node {
+struct Node
+{
 public:
 	T value;
-	Node* left;
-	Node* right;
+	Node *left;
+	Node *right;
 
-	Node(T val) {
+	Node(T val)
+	{
 		this->value = val;
 		this->left = nullptr;
 		this->right = nullptr;
@@ -34,81 +35,108 @@ public:
  * Binary Search Tree (BST) class implementation
  */
 template <typename T>
-class BST {
+class BST
+{
 
 protected:
-	Node<T>* _root;         // Root of the tree nodes
+	Node<T> *_root; // Root of the tree nodes
 
 	/* Add new T val to the tree */
-	void addHelper(Node<T>* root, T val) {
-		if (root->value > val) {
-			if (!root->left) {
+	void addHelper(Node<T> *root, T val)
+	{
+		if (root->value > val)
+		{
+			if (!root->left)
+			{
 				root->left = new Node<T>(val);
 			}
-			else {
+			else
+			{
 				addHelper(root->left, val);
 			}
 		}
-		else {
-			if (!root->right) {
+		else
+		{
+			if (!root->right)
+			{
 				root->right = new Node<T>(val);
 			}
-			else {
+			else
+			{
 				addHelper(root->right, val);
 			}
 		}
 	}
 
 	/* Print tree out in inorder (A + B) */
-	void printInOrderHelper(Node<T>* root) {
-		if (!root) return;
+	void printInOrderHelper(Node<T> *root)
+	{
+		if (!root)
+			return;
 		printInOrderHelper(root->left);
 		cout << root->value << ' ';
 		printInOrderHelper(root->right);
 	}
 
 	/* Return number of nodes in tree */
-	int nodesCountHelper(Node<T>* root) {
-		if (!root) {
+	int nodesCountHelper(Node<T> *root)
+	{
+		if (!root)
+		{
 			return 0;
 		}
-		else {
+		else
+		{
 			return 1 + nodesCountHelper(root->left) + nodesCountHelper(root->right);
 		}
 	}
 
 	/* Return height of tree (root == nullptr -> 0) */
-	int heightHelper(Node<T>* root) {
-		if (!root) {
+	int heightHelper(Node<T> *root)
+	{
+		if (!root)
+		{
 			return -1;
 		}
-		else {
+		else
+		{
 			return 1 + max(heightHelper(root->left), heightHelper(root->right));
 		}
 	}
 
 	/* Delete a given <T> value from tree */
-	bool deleteValueHelper(Node<T>* parent, Node<T>* current, T value) {
-		if (!current) return false;
-		if (current->value == value) {
-			if (current->left == nullptr || current->right == nullptr) {
-				Node<T>* temp = current->left;
-				if (current->right) temp = current->right;
-				if (parent) {
-					if (parent->left == current) {
+	bool deleteValueHelper(Node<T> *parent, Node<T> *current, T value)
+	{
+		if (!current)
+			return false;
+		if (current->value == value)
+		{
+			if (current->left == nullptr || current->right == nullptr)
+			{
+				Node<T> *temp = current->left;
+				if (current->right)
+					temp = current->right;
+				if (parent)
+				{
+					if (parent->left == current)
+					{
 						parent->left = temp;
 					}
-					else {
+					else
+					{
 						parent->right = temp;
 					}
 				}
-				else {
+				else
+				{
 					this->_root = temp;
 				}
 			}
-			else {
-				Node<T>* validSubs = current->right;
-				while (validSubs->left) {
+			else
+			{
+				Node<T> *validSubs = current->right;
+				while (validSubs->left)
+				{
 					validSubs = validSubs->left;
 				}
 				T temp = current->value;
@@ -120,33 +148,46 @@ protected:
 			return true;
 		}
 		return deleteValueHelper(current, current->left, value) ||
-			deleteValueHelper(current, current->right, value);
+					 deleteValueHelper(current, current->right, value);
+	}
+
+	void deleteSubTree(Node<T> *root)
+	{
+		if (root == nullptr)
+			return; // Nothing to delete
+		deleteSubTree(root->left);
+		deleteSubTree(root->right);
+		delete root; // Delete the root after deleting all other trees
 	}
 
 	/********************************* PUBLIC API *****************************/
 public:
-
-	BST() : _root(nullptr) { }               // Basic initialization constructor
+	BST() : _root(nullptr) {} // Basic initialization constructor
 
 	/**
 	 * Destructor - Needs to free *all* nodes in the tree
 	 * TODO: Implement Destructor
 	 */
-	~BST() {
-	    cout << "TODO: Implement Destructor" << endl;
+	~BST()
+	{
+		deleteSubTree(this->_root); // Delete the root on destruction
 	}
 
 	/* Public API */
-	void add(T val) {
-		if (this->_root) {
+	void add(T val)
+	{
+		if (this->_root)
+		{
 			this->addHelper(this->_root, val);
 		}
-		else {
+		else
+		{
 			this->_root = new Node<T>(val);
 		}
 	}
 
-	void print() {
+	void print()
+	{
 		printInOrderHelper(this->_root);
 	}
 
@@ -154,15 +195,18 @@ public:
 	 * Print the nodes level by level, starting from the root
 	 * TODO: Implement printLevelOrder
 	 */
-	void printLevelOrder() {
+	void printLevelOrder()
+	{
 		cout << "TODO: Implement printLevelOrder" << endl;
 	}
 
-	int nodesCount() {
+	int nodesCount()
+	{
 		return nodesCountHelper(this->_root);
 	}
 
-	int height() {
+	int height()
+	{
 		return heightHelper(this->_root);
 	}
 
@@ -170,11 +214,13 @@ public:
 	 * Print the maximum path in this tree
 	 * TODO: Implement printMaxPath
 	 */
-	void printMaxPath() {
+	void printMaxPath()
+	{
 		cout << "TODO: Implement printMaxPath" << endl;
 	}
 
-	bool deleteValue(T value) {
+	bool deleteValue(T value)
+	{
 		return this->deleteValueHelper(nullptr, this->_root, value);
 	}
 
@@ -182,11 +228,11 @@ public:
 	 * Find if the BST contains the value
 	 * TODO: Implement contains
 	 */
-	bool contains(T value) {
-	    cout << "TODO: Implement contains" << endl;
+	bool contains(T value)
+	{
+		cout << "TODO: Implement contains" << endl;
 		return numeric_limits<T>::min();
 	}
 };
 
 #endif
-
