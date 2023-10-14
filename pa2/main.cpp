9f75ebd3e8e53a9aa7147a43c10e9635160c92e6
Benjamin Swanzey <swanzeyb2001@gmail.com>
2023-10-13 20:12:45 -0700

    Finish tests

--- pa2/main.cpp
+++ pa2/main.cpp
@@ -34,5 +34,44 @@ int main()
   vector<int> random = nums;
   shuffle(random.begin(), random.end(), randomEngine);
 
+  // Create some test trees and fill them with the numbers
+  AVLTree<int> ascendingTree;
+  AVLTree<int> descendingTree;
+  AVLTree<int> randomTree;
+
+  for (int i = 0; i < nums.size(); i++)
+  {
+    ascendingTree.insert(ascending[i]);
+    descendingTree.insert(descending[i]);
+    randomTree.insert(random[i]);
+  }
+
+  // cout the heights of the trees, validate the trees
+  cout << "Height of Tree 1 (ascending): " << ascendingTree.height() << endl;
+  cout << "Height of Tree 2 (descending): " << descendingTree.height() << endl;
+  cout << "Height of Tree 3 (random): " << randomTree.height() << endl;
+  cout << "Validation for Tree 1 (ascending): " << ascendingTree.validate() << endl;
+  cout << "Validation for Tree 2 (descending): " << descendingTree.validate() << endl;
+  cout << "Validation for Tree 3 (random): " << randomTree.validate() << endl;
+
+  // Testing contains
+  for (int i = 1; i < 100; i += 2)
+  {
+    bool containsInTree1 = ascendingTree.contains(i);
+    bool containsInTree2 = descendingTree.contains(i);
+    bool containsInTree3 = randomTree.contains(i);
+
+    if (i % 2 == 1 && (!containsInTree1 || !containsInTree2 || !containsInTree3))
+    {
+      cout << "My AVL tree implementation is wrong" << endl;
+      break;
+    }
+    else if (i % 2 == 0 && (containsInTree1 || containsInTree2 || containsInTree3))
+    {
+      cout << "My AVL tree implementation is wrong" << endl;
+      break;
+    }
+  }
+
   return 0;
 }
